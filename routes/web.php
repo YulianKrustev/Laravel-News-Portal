4433edeeaa97a6400d7cefe64d8c5e573b5c49d5
Julian Krustev <juliankrustev2018@gmail.com>
2023-03-24 19:05:15 +0200

    Added backend sidebar category menu

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\Frontend\IndexController;
+use App\Http\Controllers\Backend\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,14 +40,33 @@ Route::middleware(['auth'])->group(function() {
 require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function() {
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/admin/loguot', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
-Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name('admin.update.password');
+
+// Admin all Routes
+Route::controller(AdminController::class)->group(function(){
+Route::get('/admin/dashboard','AdminDashboard')->name('admin.dashboard');
+Route::get('/admin/loguot','AdminLogout')->name('admin.logout');
+Route::get('/admin/profile','AdminProfile')->name('admin.profile');
+Route::post('/admin/profile/store','AdminProfileStore')->name('admin.profile.store');
+Route::get('/admin/change/password','AdminChangePassword')->name('admin.change.password');
+Route::post('/admin/update/password','AdminUpdatePassword')->name('admin.update.password');
+});
+
 }); // End Admin Middleware
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->middleware(RedirectIfAuthenticated::class)->name('admin.login');
 
 Route::get('/admin/logout/page', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
+
+
+
+
+Route::middleware(['auth','role:admin'])->group(function() {
+
+// Category all Routes
+Route::controller(CategoryController::class)->group(function(){
+
+Route::get('/all/category','AllCategory')->name('all.category');
+
+});
+
+}); // End Admin Middleware
